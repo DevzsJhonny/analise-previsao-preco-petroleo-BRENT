ba43ad80c895953584cae835f7cafbe73cf5dd74
Alexanderlcs01 <100323247+Alexanderlcs01@users.noreply.github.com>
2024-12-01 13:23:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+# Importando as Bibliotecas
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
-
+import matplotlib.pyplot as plt
+import io
+from statsmodels.tsa.seasonal import seasonal_decompose
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from prophet import Prophet
 from datetime import date
 from streamlit_option_menu import option_menu
 
-# Configurações gerais
+# Configurações gerais da pagina do streamlit
 st.set_page_config(layout="centered", page_title="Análise do Preço do Petróleo")
 
-#Carregando dados
+# Carregando dados
 dados = pd.read_excel("Base_Tech.xlsx", parse_dates=["data"])
 dados.rename(columns={"data": "ds", "preco": "y"}, inplace=True)
 dados["ds"] = pd.to_datetime(dados["ds"])
@@ -21,9 +24,15 @@ dados["media"] = dados.groupby("ano")["y"].transform("mean").round(2)
 dados["y"] = dados["y"].round(2)
 dados.sort_values(by="ds", ascending=True, inplace=True)
 
+dados_s = pd.read_excel("Base_Tech.xlsx", parse_dates=["data"])
+dados_s.rename(columns={"data": "ds", "preco": "y"}, inplace=True)
+dados_s = dados_s.set_index('ds', inplace=True)
+
+# Carregando os dados dos derivados do petróleo
 dados_dv = pd.read_excel("Base_Dv.xlsx", parse_dates=["ds"])
 dados_dv.sort_values(by="ds", ascending=True, inplace=True)
 
+# Adicionando as colunas de média e ano  para cada derivado
 dados_dv_ano = dados_dv.copy()
 dados_dv_ano["ano"] = dados_dv["ds"].dt.year
 dados_dv_ano = dados_dv_ano.drop(columns=['ds'])
@@ -47,7 +56,7 @@ if "data_fim" not in st.session_state:
 with st.sidebar:
     selected = option_menu(
         menu_title="Menu",
-        options=["Participantes", "Introdução", "Desenvolvimento", "Paineis Interativos", "Referências"],
+        options=["Participantes", "Introdução", "Desenvolvimento", "Paineis Interativos"],
         icons=["people-fill", "book-fill", "book-fill", "bar-chart-fill", "list-columns-reverse"],
         menu_icon="grid-fill",
         default_index=0,
@@ -89,44 +98,44 @@ if selected == "Introdução":
     st.markdown("""
 
 
-    # Análise e Previsão do Petróleo
-    #### Utilizamos o modelo de Predição Prophet para tentar prever o preço diário do petróleo em (U$D)
-                
-    __Este projeto é para fins educativos, não recomendamos como investimento de qualquer natureza.__
-    
-    # O petróleo Brent 
-    
-    O Petróleo Brent é denominado assim por ser extraído de uma plataforma da Shell no Mar do Norte e que leva este mesmo nome.
-    A Shell costumava nomear seus campos de exploração fazendo referência a aves aquáticas como o ganso-bravo (brent goose). <br>
-    O barril **Brent** é o valor de referência mundial, usado, inclusive, pela política de preços da Petrobras. 
-    É um tipo de petróleo bruto leve e doce e que possui baixo teor de enxofre e densidade, extraído do Mar do Norte, entre a Noruega e o Reino Unido. 
-    Ele serve como referência global para precificação de mais de dois terços do petróleo comercializado no mundo, sendo fundamental para a economia global 
-    e influenciando diversos setores, desde combustíveis até produtos petroquímicos. <br>
-    Servindo como padrão global para precificação de outros tipos de petróleo, como o WTI (West Texas Intermediate), outro tipo de petróleo e barril, dos Estados Unidos. <br>
-    O Brent é usado para calcular o valor de aproximadamente dois terços do petróleo comercializado no mundo e é conhecido por ter uma menor densidade e por ter baixo teor de enxofre, características que o tornam ideal para refino 
-    de combustíveis como gasolina e diesel.
-    
-    """, unsafe_allow_html=True)
+        # Análise e Previsão do Petróleo
+        #### Utilizamos o modelo de Predição Prophet para tentar prever o preço diário do petróleo em Dólares (U$D)
+                    
+        _Este projeto é para fins educativos, não recomendamos como investimento de qualquer natureza._
+        
+        # O petróleo Brent 
 
+        O Petróleo Brent é denominado assim por ser extraído de uma plataforma da Shell no Mar do Norte e que leva este mesmo nome.
+        A Shell costumava nomear seus campos de exploração fazendo referência a aves aquáticas como o ganso-bravo (brent goose). <br>
+        O barril *Brent* é o valor de referência mundial, usado inclusive pela política de preços da Petrobras. É um tipo de petróleo 
+        bruto leve e doce e que possui baixo teor de enxofre e densidade, características que o tornam ideal para refino 
+        de combustíveis como gasolina e diesel, e é extraído do Mar do Norte, entre a Noruega e o Reino Unido. <br>
+        Ele serve como referência global para precificação de mais de dois terços do petróleo comercializado no mundo, sendo fundamental 
+        para a economia global e influenciando diversos setores, desde combustíveis até produtos petroquímicos. 
+        É padrão também para outros tipos de petróleo como o WTI (West Texas Intermediate) dos Estados Unidos. <br>
+        
+        """, unsafe_allow_html=True)
+    
+    # Apresentação da imagem do navio
     st.image("petroleo.jpg", caption="Navio Plataforma", use_column_width=True)
 
 # Seção: Desenvolvimento
 if selected == "Desenvolvimento":
     st.title("Análise do Preço do Petróleo")
-
+                
     st.markdown("""
                 
-    Realizamos a utilizacao dos dados que foram disponibilizados pelo IPEA. <br>
-    A base de dados conta com o histórico desde 1987 até os dias de hoje, tendo resultados consisos sobre o preço do petróleo.<br>
-    Para avaliar o modelo de machine learning desenvolvido para prever o valor diário (utilizando séries temporais), 
-    empregamos dados de 2018 a 2023. Apesar da base de dados possuir mais de 30 anos, este período foi selecionado para a análise, 
-    permitindo a comparação dos resultados do modelo com os dados reais de 2024.
-
-    A utilização do periodo de 2018 à outubro de 2024 se justifica pelos fatores da crise econômica como por exemplo, a Grande Recessão de 2008 ou geopolíticas como a Guerra do Iraque de 2003.
-    Estes fatos ocorream em decadas anteriores e causaram uma grande volatilidade no preço do petróleo.
-
-    Tendo isso em mente, Consideramos que uma analise de um curto período já surja efeito em nossa análise, visto que nos anos de  2020 ocorreu a pandemia da COVID-19 e em nossa anlise
-    é possive identificar que estefator impactou no preço do petróleo e também é valido citar a guerra da Ucrania e Russia que ainda ocorre atualmente.
+    Realizamos a utilização dos dados que foram disponibilizados pelo IPEA (Instituto de Pesquisa Econômica Aplicada). <br>
+    <br>
+    A base de dados conta com o histórico desde 1987 até outubro de 2024.<br>
+    <br>
+    Para avaliar o modelo de machine learning desenvolvido para prever o valor diário (utilizando séries temporais), empregamos dados de 2018 a 2024. Apesar da base de dados possuir mais de 30 anos, esse período foi selecionado para nossa análise, permitindo que nosso modelo tenha resultados mais concisos e uma boa performance no fechamento diário.<br>
+    <br>
+    A utilização do período de 2018 à outubro de 2024 se justifica pelos fatores da crise econômica como por exemplo a Grande Recessão de 2008, ou fatores geopolíticos como a Guerra do Iraque de 2003.<br>
+    <br>
+    Esses fatos ocorreram em décadas anteriores e causaram uma grande volatilidade no preço do petróleo. <br>
+    <br>
+    Tendo isso em mente, consideramos que um curto período já surja efeito em nossa análise, visto que tivemos grandes acontecimentos como a pandemia da COVID-19 em 2020 que impactou bastante no preço do petróleo, e em 2022 o conflito entre Ucrânia e Rússia. Essa guerra que ainda não chegou ao fim continua impactando nos preços.<br>
                             
     """, unsafe_allow_html=True)
 
@@ -136,33 +145,49 @@ if selected == "Desenvolvimento":
     
     st.markdown("""
 
-                Alguns fos fatores que acabam afetando o preço do petróleo são: <br>
-                - **Conflitos Geopoliticos, sanções, crescimento economico global e sazonalidades** <br>
-                Muitos dos principais países produtores de petróleo estão em áreas politicamente instáveis, como a Venezuela e o Irã. 
-                Esse tipo de incerteza impacta diretamente a cotação do petróleo, especialmente quando os governos desses países tomam ações inesperadas envolvendo a política global.
+                Alguns dos fatores que afetaram o preço do petróleo nos últimos anos: <br>
+                - *Conflitos Geopolíticos, sanções, crescimento econômico global e sazonalidades* <br>
+                Muitos dos principais países produtores de petróleo estão em áreas politicamente instáveis, como a Venezuela e o Irã, e isso pode impactar a cotação do petróleo, principalmente quando os governos desses países tomam ações inesperadas envolvendo a política global.<br>
 
-                - **Catástrofes ambientais** <br>
-                Catastrofes como furacões em paises que são produtores de petróleo também impacta negativamente ao preço do petróleo.
-                Por exemplo os furaçõesvno Golfo do México, essas catastrofes ambientais fazem com que seja necessário interromper à 
-                extração do petróleo e gás natural (que também é uma fonte de combustível)A região é responsável por uma grande parte da 
-                produção de petróleo e gás natural dos Estados Unidos, com centenas de plataformas de perfuração operando lá.
-                
+                - *Catástrofes ambientais* <br>
+                Catástrofes como furacões em países que são produtores de petróleo também impactam negativamente o preço do petróleo. Por exemplo os furações no Golfo do México, algumas dessas catástrofes ambientais fazem com que seja necessário interromper as extrações de petróleo e gás natural (que também é uma fonte de combustível). A região é responsável por uma grande parte da produção de petróleo e gás natural dos Estados Unidos, com centenas de plataformas de perfuração operando lá.<br>
 
-                - **Guerras**<br>
-                Sobre os fatores de guerra, podemos citar a guerra da Ucrânia e Russia. Iniciada em 02/2022.
-                A Rússia é um dos maiores produtores de petróleo do mundo, sempre estando entre os 3 maiores do mundo estando atrás dos Estados Unidos e Arábia Saldita.
-                Já a Ucrânia não produz tanto petróleo, estando por volta de 50 a 70 mil barris por dia.
-                A produção de petróleo na Ucrânia é muito pequena em comparação com a Rússia, que continua a ser uma potência energética global.
-                No entanto, a Ucrânia desempenha um papel importante no transporte de energia, o que afeta os fluxos de petróleo e gás entre a Rússia e a Europa.
+                - *Guerras*<br>
+                A guerra entre Ucrânia e Rússia, iniciada em 02/2022 já impactou bastante na variação de preços do petróleo. A produção de petróleo na Ucrânia é muito pequena em comparação com a Rússia, que continua a ser uma potência energética global, mas embora a produção da Ucrânia esteja por volta de 50 a 70 mil barris por dia, ela desempenha um papel importante no transporte de energia, o que afeta os fluxos de petróleo e gás entre a Rússia e a Europa. Já a Rússia é um dos três maiores produtores de petróleo do mundo, ficando atrás apenas dos Estados Unidos e da Arábia Saudita.<br>
 
-
-                - **Pandemias** <br>
-                A pandemia de COVID-19, iniciada em meados de 2020, impactou fortemente nos preços. 
-                O preço estava entre 70,25 dólares no início do mês de janeiro e atingiu o menor valor que ocorreu em 21 de Abril de 2020 com preço em  9,12 dólares.
+                - *Pandemias* <br>
+                A pandemia de COVID-19, iniciada em meados de 2020, impactou fortemente nos preços do petróleo, principalmente no início deste período. O preço estava entre 70,25 dólares no início do mês de janeiro deste ano e atingiu o menor valor de 2020 em 21 de abril com preço em 9,12 dólares.<br>
                 
     """, unsafe_allow_html=True)
 
-    # Seção: Paineis
+    # Garantindo que dados_s seja uma série temporal válida
+    dados_s = dados.set_index('ds')['y']  # Agora 'dados_s' é uma série temporal com 'ds' como índice e 'y' como valores
+
+    # Decomposição sazonal
+    resultado = seasonal_decompose(dados_s, model='multiplicative', period=120)
+
+    # Título e descrição adicional
+    st.markdown("""
+    ### Decomposição Sazonal do Preço do Petróleo
+    A decomposição sazonal ajuda a entender melhor os padrões sazonais nos preços do petróleo ao longo do tempo. A decomposição divide os dados em componentes como tendência, sazonalidade e resíduos, o que pode fornecer insights sobre os fatores que afetam as flutuações nos preços.
+    """, unsafe_allow_html=True)
+
+    st.subheader("Componente de Tendência")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=resultado.trend.index, y=resultado.trend.dropna(), mode='lines', name='Trend'))
+    st.plotly_chart(fig)
+
+    st.subheader("Componente Sazonal")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=resultado.seasonal.index, y=resultado.seasonal.dropna(), mode='lines', name='Trend'))
+    st.plotly_chart(fig)
+
+    st.subheader("Resíduos")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=resultado.resid.index, y=resultado.resid.dropna(), mode='lines', name='Trend'))
+    st.plotly_chart(fig)
+
+# Seção: Paineis
 if selected == "Paineis Interativos":
     if submenu == "Apresentação Geral":
         st.title("Visualização dos Dados")
@@ -222,6 +247,7 @@ if selected == "Paineis Interativos":
         st.title("Previsão dos Dados")
 
         st.write("""
+                 
         Aqui é o local em que apresentaremos previsões utilizando o modelo PROPHET. <br>
         Apresentamos toda a base de dados no primeiro gráfico, que é de 01/2018 até 10/2024.
         Iniciamos a previsão em 01/11/24 e conseguimos prever os valores (com certa margem de erro) até 03/26.
@@ -296,9 +322,7 @@ if selected == "Paineis Interativos":
 
         grid = grafico[['ds','yhat']]
         grid['ds'] = pd.to_datetime(grid['ds'], format='%d/%m/%Y')
-        grid.rename(columns={'yhat': 'Valor Predito'}, inplace=True)
-        
-        grid = grid.set_index('ds')
+        grid.rename(columns={'ds': 'Data', 'yhat': 'Valor Predito'}, inplace=True)
 
         # Gráfico com cores distintas
         grafico_1 = go.Figure()
@@ -317,11 +341,29 @@ if selected == "Paineis Interativos":
         prophet_m = calculate_metrics(teste['y'], forecast_teste['yhat'].values)
         print_metrics(prophet_m)
 
-        st.markdown("<center><br><br><h3>Tabela para exportação dos dados Preditos </h3> </center> <br>", unsafe_allow_html=True)
-        
-        st.table(grid)
+        # Exemplo de dataframe que você deseja exportar (substitua por seu dataframe real)
+        df_exportar = grid  # Aqui 'grid' é o dataframe que você quer exportar
 
-        
+        # Cria um buffer em memória para o arquivo Excel
+        output = io.BytesIO()
+
+        # Escreve o dataframe no arquivo Excel com openpyxl
+        with pd.ExcelWriter(output, engine="openpyxl") as writer:
+            df_exportar.to_excel(writer, index=False, sheet_name="Dados")
+
+        # Rewind o buffer para que possa ser lido
+        output.seek(0)
+
+        # Exibe o botão para download
+        st.markdown("<center><h3>Exportar Tabela para Excel</h3></center>", unsafe_allow_html=True)
+
+        st.download_button(
+            label="Baixar Dados em Excel",
+            data=output,
+            file_name="dados_previsao_petroleo.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+
 
     # Seção: Desenvolvimento
     if submenu == "Derivados":
@@ -332,16 +374,17 @@ if selected == "Paineis Interativos":
         Agência Nacional do Petróleo, Gás Natural e Biocombustíveis, que acompanha através de pesquisa semanal
         os preços praticados por revendedores de combustíveis automotivos e de gás liquefeito de petróleo envasilhado em botijões,
         buscamos os dados detalhados por cada revendedor em cada cidade de todos os estados, e aplicamos uma média diária de preço de venda por produto. <br><br>
+                    
+        Considerando a base de dados da ANP, disponível por semestres, entre Janeiro de 2020 até Junho de 2024, que é o último período disponível. <br><br>
+        
         Os resultados estão representados nos gráficos abaixo.
         """, unsafe_allow_html=True)
 
         # Inicializar valores padrão no session_state
         if "data_inicio_ano" not in st.session_state:
-            st.session_state.data_inicio_ano = date(2019, 1, 1)
+            st.session_state.data_inicio_ano = date(2020, 1, 1)
         if "data_fim_ano" not in st.session_state:
             st.session_state.data_fim_ano = date(2024, 10, 31)
-
-        
 
         intervalo_datas = st.date_input(
             "Intervalo de datas",
@@ -356,7 +399,6 @@ if selected == "Paineis Interativos":
             default=["Diesel","Diesel S10","Etanol","Gasolina","Gasolina Aditivada","GNV"]  # Valor(es) padrão
         )
 
-
         st.session_state.data_inicio_ano, st.session_state.data_fim_ano = intervalo_datas
 
         # Filtrar os dados com base no intervalo
@@ -369,14 +411,16 @@ if selected == "Paineis Interativos":
         fig_preco_dv = px.line(df_dv_filtrado, x="ds", y=input_dv, title="Variação do Preço dos Derivados do Petróleo")
         st.plotly_chart(fig_preco_dv)
 
+        # Ajustando as nomenclaturas dos eixos x e y
         fig_preco_dv.update_layout(
             xaxis_title="Data de Referência",
             yaxis_title="Preço (em US$)",
             template="plotly_white"
         )
-
+        # Lista da Multiseleção
         lista = ["Media_Diesel", "Media_Diesel_S10", "Media_Etanol", "Media_Gasolina", "Media_Gasolina_Aditivada", "Media_GNV"]
 
+        # Multiseleção
         input_dv_ano = st.multiselect(
             "Selecione os derivados:",  # Rótulo do campo
             options=lista,             # Opções disponíveis
@@ -416,7 +460,3 @@ if selected == "Paineis Interativos":
 
         # Exibir o gráfico
         st.plotly_chart(fig_preco_dv_ano)
-
-# Seção: Referências
-if selected == "Referências":
-    st.write('Teste')
